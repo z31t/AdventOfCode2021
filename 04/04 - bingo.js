8cae23991b31dbe493b3f5335e1b4a719295d6f8
frosthammer <code@dailyshit.org>
2021-12-04 22:21:24 +0100

    day 04: squid bingo - part 2

--- 04/04 - bingo.js
+++ 04/04 - bingo.js
@@ -70,18 +70,58 @@ function doBingo(data) {
 		return newBingoList;
 	}
 
+	var stopPart1 = false;
 	chosenNumbers.forEach(chosenNumber => {
+		if (stopPart1) {
+			return;
+		}
 		bingoList = markChosenNumber(bingoList, chosenNumber);
 		var score = checkWin(bingoList, chosenNumber);
 		if (score != null) {
 			console.log("score: " + score);
-			process.exit();
+			stopPart1 = true;
 		}
 	});
 
-	bingoList.forEach(bingo => {
-		console.log(bingo);
-	});
+	console.log("\n\nPart 2");
 
+	function isWinning(bingo) {
+		for (var i=0; i<5; i++) {
+			var horizontalLine = [];
+			var verticalLine = [];
+
+			for (var j=0; j<5; j++) {
+				verticalLine.push(bingo[i][j]);
+				horizontalLine.push(bingo[j][i]);
+			}
+			if ((horizontalLine.filter(n => n=="X").length == 5) || (verticalLine.filter(n => n=="X").length == 5)) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	function removeWinningBoards(bingoList) {
+		var newBingoList = [];
+		bingoList.forEach(bingo => {
+			if (isWinning(bingo)) {
+				return;
+			}
+			newBingoList.push(bingo);
+		});
+		return newBingoList;
+	}
+
+	var lastScore = null;
+	chosenNumbers.forEach(chosenNumber => {
+		console.log("active bingo sheets: %d", bingoList.length);
+		if (bingoList.length==0) {
+			return;
+		}
+		bingoList = markChosenNumber(bingoList, chosenNumber);
+		lastScore = checkWin(bingoList, chosenNumber);
+		bingoList = removeWinningBoards(bingoList);
+	});
+	console.log("last score: " + lastScore);
 
 }
